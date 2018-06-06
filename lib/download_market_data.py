acc6850a635ef643648c4962030a3729f6bf5b25
kzhang8975 <kelvin.zhangyi@gmail.com>
2018-06-06 22:45:57 +0800

    fix

--- lib/download_market_data.py
+++ lib/download_market_data.py
@@ -2,7 +2,7 @@ import time
 import pandas as pd
 import datetime as dt
 
-from utils import save_symbol_fullbook_data
+from utils import save_fullbook_data
 
 _dt_dt_ = dt.datetime
 _dt_td_ = dt.timedelta
@@ -11,25 +11,30 @@ _dt_t_  = dt.time
 
 from binance.client import Client
 
-def get_market_data(client, symbol, end_time, delay=1):
+def get_market_data(end_time, delay=1):
+    client = Client(api_key, api_secret)
     fullbook = pd.DataFrame()
+    
+    ticker_list = [x['symbol'] for x in client.get_all_tickers()]
+    
     while(_dt_dt_.now() < end_time):
-        try:
-            depth   = pd.DataFrame(client.get_order_book(symbol=symbol))
+        for symbol in ticker_list:
+            try:
+                depth   = pd.DataFrame(client.get_order_book(symbol=symbol))
 
-            book_update = pd.DataFrame({_dt_dt_.now().strftime("%Y%m%d %H:%M:%S.%f"): {'q_bid': float(depth.loc[0, 'bids'][1]), 'p_bid': float(depth.loc[0, 'bids'][0]), 
-                                       'q_ask': float(depth.loc[0, 'asks'][1]), 'p_ask': float(depth.loc[0, 'asks'][0]), 
-                                       'q_bid_2': float(depth.loc[1, 'bids'][1]), 'p_bid_2': float(depth.loc[1, 'bids'][0]), 
-                                       'q_ask_2': float(depth.loc[1, 'asks'][1]), 'p_ask_2': float(depth.loc[1, 'asks'][0])
-                                       }})
+                book_update = pd.DataFrame({_dt_dt_.now().strftime("%Y%m%d %H:%M:%S.%f"): {'symbol': symbol, 'q_bid': float(depth.loc[0, 'bids'][1]), 'p_bid': float(depth.loc[0, 'bids'][0]), 
+                                           'q_ask': float(depth.loc[0, 'asks'][1]), 'p_ask': float(depth.loc[0, 'asks'][0]), 
+                                           'q_bid_2': float(depth.loc[1, 'bids'][1]), 'p_bid_2': float(depth.loc[1, 'bids'][0]), 
+                                           'q_ask_2': float(depth.loc[1, 'asks'][1]), 'p_ask_2': float(depth.loc[1, 'asks'][0])
+                                           }})
 
-            fullbook = fullbook.append(book_update.T)
-        except:
-            pass
+                fullbook = fullbook.append(book_update.T)
+            except:
+                pass
 
         time.sleep(delay)
 
-    save_symbol_fullbook_data(symbol, fullbook, '/home/yizhang/Research/data/%s' % (_dt_dt_.now().strftime("%Y/%m/%d/")))
+    save_fullbook_data(fullbook, '/home/yizhang/Research/data/%s' % (_dt_dt_.now().strftime("%Y/%m/%d/")))
 
 
 if __name__ == "__main__":
@@ -37,10 +42,5 @@ if __name__ == "__main__":
     api_key      = api_key_info.loc[0, 'value']
     api_secret   = api_key_info.loc[1, 'value']
     
-    client = Client(api_key, api_secret)
-    
-    ticker_list = [x['symbol'] for x in client.get_all_tickers()]
-    
-    for symbol in ticker_list:
-        get_market_data(client, symbol, end_time=_dt_dt_.combine(_dt_d_.today(), _dt_t_(10,59)))
+    get_market_data(end_time=_dt_dt_.combine(_dt_d_.today(), _dt_t_(23,50)))
 
--- lib/utils.py
+++ lib/utils.py
@@ -1,12 +1,12 @@
 import errno    
 import os
 
-def save_symbol_fullbook_data(symbol, fullbook_data, path):
+def save_fullbook_data(fullbook_data, path):
     try:
         os.makedirs(path)
-        fullbook_data.to_csv(path + '/' + '%s.csv' % symbol)
+        fullbook_data.to_csv(path + '/' + 'market_data.csv')
     except OSError as exc:  # Python >2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
-            fullbook_data.to_csv(path + '/' + '%s.csv' % symbol)
+            fullbook_data.to_csv(path + '/' + 'market_data.csv')
         else:
             raise
